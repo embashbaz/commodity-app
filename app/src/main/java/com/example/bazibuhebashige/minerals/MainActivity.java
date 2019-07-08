d91655582f90468e4c92b2ed68aac734f5f45f74
emery bashige <bemeryb@gmail.com>
2019-07-08 22:40:52 +0300

    added a searchView to get specific product

--- app/src/main/java/com/example/bazibuhebashige/minerals/MainActivity.java
+++ app/src/main/java/com/example/bazibuhebashige/minerals/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.bazibuhebashige.minerals;
 
 import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import com.example.bazibuhebashige.minerals.comodities_adapter.adaptateronclickhandler;
 
 import static com.example.bazibuhebashige.minerals.products.*;
@@ -41,4 +46,29 @@ public class MainActivity extends AppCompatActivity implements adaptateronclickh
         startActivity(intent);
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.search, menu);
+        final MenuItem searchitem = menu.findItem(R.id.action_search);
+        final SearchView searchView =(SearchView) searchitem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                comodities.getFilter().filter(s);
+
+
+                return true;
+            }
+        });
+
+        return true;
+    }
 }
--- app/src/main/java/com/example/bazibuhebashige/minerals/comodities_adapter.java
+++ app/src/main/java/com/example/bazibuhebashige/minerals/comodities_adapter.java
@@ -7,7 +7,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 import static android.content.ContentValues.TAG;
 import static com.example.bazibuhebashige.minerals.products.someofthem;
@@ -17,7 +26,28 @@ import static com.example.bazibuhebashige.minerals.products.someofthem;
 
 
 
-public class comodities_adapter extends RecyclerView.Adapter<comodities_adapter.myviewholder> {
+public class comodities_adapter extends RecyclerView.Adapter<comodities_adapter.myviewholder> implements Filterable {
+
+    //String[][]oneofthem=someofthem;
+
+    public static List<String> flatten(String[][] data) {
+        List<String> list = new ArrayList<>();
+
+        for(int i = 0; i < data.length; i++) {
+            for(int j = 0; j < data[i].length; j++){
+                list.add(data[i][0]);
+            }
+        }
+
+        return list;
+    }
+
+
+    private List<String> all = flatten(someofthem);
+    private List<String> some= new ArrayList<>(all);
+
+
+
 
 
     public interface adaptateronclickhandler {
@@ -49,7 +79,7 @@ public class comodities_adapter extends RecyclerView.Adapter<comodities_adapter.
         @Override
         public void onClick(View v) {
             int adapterPosition = getAdapterPosition();
-            String specific = someofthem[adapterPosition][0];
+            String specific = all.get(adapterPosition);
             mclickHandler.onClick(specific);
         }
        }
@@ -67,14 +97,50 @@ public class comodities_adapter extends RecyclerView.Adapter<comodities_adapter.
    }
    @Override
     public  void onBindViewHolder(myviewholder holder, int position) {
-       String initInfo = someofthem[position][0];
+       String initInfo = all.get(position);
        holder.comtextview.setText(initInfo);
 
        specmin = initInfo;
    }
    @Override
 
-    public int getItemCount (){return someofthem.length;}
+    public int getItemCount (){return all.size();}
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<String> filtedlist = new ArrayList<>();
+
+            if(constraint==null || constraint.length()==0)
+                filtedlist.addAll(some);
+            else{
+                String filterpatern = constraint.toString().toLowerCase().trim();
+
+                for(String item: some){
+                    if(item.toLowerCase().contains(filterpatern))
+                        filtedlist.add(item);
+                }
+
+            }
+
+            FilterResults result = new FilterResults();
+            result.values = filtedlist;
+
+            return  result;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            all.clear();
+            all.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
 
 
 }
